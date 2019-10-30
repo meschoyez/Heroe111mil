da96dee566a52337c353140f27746a88c0bdde30
visita <visita@lab75.famaf.unc.edu.ar>
2019-10-30 16:33:27 -0300

    Se establece el alcance y danio del hacha

---
+++ src/Heroe/Hacha.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Heroe;
+
+/**
+ *
+ * @author visita
+ */
+public class Hacha extends Armas {
+    
+    public Hacha () {
+    
+        super.setAlcance(50);
+        super.setDanio(30);
+        
+        
+    }
+    
+
+    @Override
+    public void Atacar() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void RecargarArma(Objetos obj) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
