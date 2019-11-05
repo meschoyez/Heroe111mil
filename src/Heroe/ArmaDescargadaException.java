480fc9230d94800bc40208e0a0279c83eaaf70d3
Maximiliano Eschoyez <maximiliano.eschoyez@unc.edu.ar>
2019-11-05 15:31:25 -0300

    Revision Botella e implementacion ArmaDescargadaException

--- src/Heroe/ArmaDescargadaException.java
+++ src/Heroe/ArmaDescargadaException.java
@@ -1,10 +1,9 @@
-package Heroe;
-
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+package Heroe;
 
 /**
  *
@@ -13,7 +12,7 @@ package Heroe;
 public class ArmaDescargadaException extends Exception {
 
     public ArmaDescargadaException (String mensaje) {
-        super (mensaje);
+        super(mensaje);
     }
-
+    
 }
