bcc4d7b32a3e12e983908e6db9bcc3e348559a5b
visita <visita@lab75.famaf.unc.edu.ar>
2019-11-05 16:15:49 -0300

    Implementacion de la clase cuchillo

---
+++ src/Heroe/Cuchillo.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Heroe;
+
+/**
+ *
+ * @author visita
+ */
+public class Cuchillo extends Armas{
+    
+    public Cuchillo () {
+        super.setNombre ("Cuchillo");
+        super.setPeso(1);
+        super.setAlcance(3);
+        super.setDanio (15);
+
+    }
+
+    public void Atacar () {
+        
+    }
+    
+    public void RecargarArma (Objetos obj) {
+        
+    }    
+
+}
